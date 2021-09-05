f117e65e2073a2e68b7252eacf3db5ffcf0198aa
old-horizon <qsd8650@hotmail.co.jp>
2021-09-05 11:17:08 +0900

    restructure modules

--- examples/docker/zalenium/build.sh
+++ examples/docker/zalenium/build.sh
@@ -3,4 +3,4 @@ set -e
 
 image=${1:-zalenium-file-download}
 root=$(git rev-parse --show-toplevel)
-DOCKER_BUILDKIT=1 docker build --rm -t $image -f $root/examples/zalenium/Dockerfile $root
+DOCKER_BUILDKIT=1 docker build --rm -t $image -f $root/examples/docker/zalenium/Dockerfile $root
